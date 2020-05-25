c5d2e31e65c26f6a7010fc55c8b7c8e2b414670c
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-05-25 10:28:22 +0000

    build based on 1214d9f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#CoffeeBeetles.jl-1","page":"Home","title":"CoffeeBeetles.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [CoffeeBeetles]","category":"page"}]
+}
