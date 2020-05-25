65ed313f785a459db874b33f32ca4c84f790f377
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-05-25 11:43:04 +0000

    build based on fc18cc1

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#CoffeeBeetles.jl-1","page":"Home","title":"CoffeeBeetles.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [CoffeeBeetles]","category":"page"}]
+[{"location":"#CoffeeBeetles.jl-1","page":"Home","title":"CoffeeBeetles.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [CoffeeBeetles]","category":"page"},{"location":"#CoffeeBeetles.main-Tuple{}","page":"Home","title":"CoffeeBeetles.main","text":"main()\n\nCreate all the tables and figures included in the manuscript.\n\n\n\n\n\n","category":"method"}]
 }
