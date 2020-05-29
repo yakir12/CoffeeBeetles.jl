8f99ccab7885dba0caa21c92acd865eea9c3af08
yakir12 <12.yakir@gmail.com>
2020-05-29 14:23:09 +0200

    add

--- examples/main.jl
+++ examples/main.jl
@@ -1,7 +1,7 @@
 import Pkg
 Pkg.Registry.add("General") # you may skip this line if this is not a fresh instalation of Julia and you've updated/added a packge before
 Pkg.Registry.add(Pkg.RegistrySpec(url = "https://github.com/yakir12/DackeLab")) # you need to do this only once for each instalation of Julia
-Pkg.add("Format2DB", "DungAnalyse") # you need to do this only once for each environment
+Pkg.add(["Format2DB", "DungAnalyse"]) # you need to do this only once for each environment
 
 path = "." # change this to the directory that contains all your experiments
 
