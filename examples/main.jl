f015f1f4597d78bea2d5c35d7b112add69719c07
yakir12 <12.yakir@gmail.com>
2020-05-29 11:31:52 +0200

    added example

---
+++ examples/main.jl
@@ -0,0 +1,25 @@
+import Pkg
+Pkg.Registry.add("General") # you may skip this line if this is not a fresh instalation of Julia and you've updated/added a packge before
+Pkg.Registry.add(Pkg.RegistrySpec(url = "https://github.com/yakir12/DackeLab")) # you need to do this only once for each instalation of Julia
+Pkg.add("Format2DB", "DungAnalyse") # you need to do this only once for each environment
+
+path = "." # change this to the directory that contains all your experiments
+
+using Format2DB, Glob, DungAnalyse, Serialization
+import Base.Threads.@spawn
+@sync foreach(readdir(glob"source_*", tempdir())) do d
+    @spawn rm(d, force = true, recursive = true)
+end
+todo = readdir(path)
+n = length(todo)
+sources = Vector{Any}(undef, n)
+@sync for i in 1:n
+    @spawn sources[i] = Format2DB.main(joinpath(path, todo[i]))
+end
+source = DungAnalyse.joinsources(sources)
+@sync foreach(sources) do d
+    @spawn rm(d, force = true, recursive = true)
+end
+data = DungAnalyse.main(source)
+serialize("data", data)
+
